ac1a5ce5ee59e35c32538c2dd8ac879fa5f27342
ПК <ekrapostin@gmail.com>
2023-03-16 02:22:29 +0300

    Выполнил первое задание

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> integers = List.of(1,2,3,99,77,55,8);
+        List<Integer> integers = List.of(1, 2, 3, 99, 77, 55, 8);
         Comparator<Integer> order = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -22,35 +22,31 @@ public class Main {
             }
         };
 
-        System.out.println(integers.stream()
-                .sorted(order)
-                .collect(Collectors.toList()));
+        BiConsumer<Integer, Integer> minMaxConsumer = (t, t2) -> System.out.println("min: " + t + " max: " + t2);
 
-        Integer min = integers.get(integers.size() - 1);
-        Integer max = integers.get(integers.get(0));
-
+        findMinMax(integers.stream(),order, minMaxConsumer);
         findEven(integers);
     }
-    //Задача 1
-    public static <T> void findMinMax(Stream <? extends T> stream,
-                                      Comparator <? super T> order,
-                                      BiConsumer<? super T, ? super T> minMaxConsumer) {
-        T min = stream
-                .min(order)
-                .get();
-        T max = stream
-                .max(order)
-                .get();
 
-        BiConsumer<T,T> biConsumer = (t, t2) -> System.out.println("min: " + t + " max: " + t2);
-  }
+    //Задача 1
+    public static <T> void findMinMax(Stream<? extends T> stream,
+                                      Comparator<? super T> order,
+                                      BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> intList = stream
+                .sorted(order)
+                .collect(Collectors.toList());
+
+        T min = intList.get(0);
+        T max = intList.get(intList.size() - 1);
+
+        minMaxConsumer.accept(min, max);
+    }
 
     //Задача 2
     public static void findEven(List<Integer> list) {
-        List <Integer> num = Arrays.asList();
         list = list
                 .stream()
-                .filter(n->n%2==0)
+                .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
         System.out.println(list);
     }
